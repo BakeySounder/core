fe095dd388114491c65ed1fecf51c53d64ece692
Bakey <klou999d@gmail.com>
2024-12-09 09:02:59 +0300

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import Body, Depends, FastAPI, HTTPException, Response
 from pydantic import BaseModel, Field, field_validator
 from dotenv import load_dotenv
 import os
-from sqlalchemy import Column, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,9 +11,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy import String
-
 import uvicorn
 import asyncio
+
+# SQL ALCHEMY MODELS
 class Base(DeclarativeBase):
   pass
 class UserModel(Base):
@@ -22,12 +23,12 @@ class UserModel(Base):
     id:Mapped[int] = mapped_column(autoincrement=True, primary_key=True, index=True)
     name:Mapped[int] = mapped_column(unique=True)
     
+    
+# PYDANTIC SCHEME    
 class UserSchema(BaseModel):
     id: int
     name: str = Field(alias = "username")
     
-
-
 load_dotenv()
 app = FastAPI()
 async_engine = create_async_engine(os.getenv("DATABASE_URL"))
@@ -39,18 +40,15 @@ async def get_session():
     yield session
         
 
-
 @app.post("/api/")
 async def root(response: Response, user: UserSchema, session: AsyncSession = Depends(get_session) ):
-  # raise HTTPException(status_code=404, detail="Item not found")
   res = await session.scalars(select(UserModel).filter(UserModel.name == user.name))
   return res.all()
 
 async def main():
-  async with async_engine.connect() as conn:
+  async with async_engine.begin() as conn:
     await conn.run_sync(Base.metadata.drop_all)
     await conn.run_sync(Base.metadata.create_all)
-    await conn.commit()
   uvicorn.run("main:app", host="localhost", port=8000, reload=True)
 
 if __name__ == "__main__":
